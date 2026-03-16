a84c33a686e37350b355455369e9b6691e60afd2
Darya Korobeynikova <dasha_kord@rambler.ru>
2026-03-16 19:09:26 +0700

    2лабораторная

--- SmartPlate/Recipes/views.py
+++ SmartPlate/Recipes/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.template.loader import render_to_string
 
-
+#####################
 # Create your views here.
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
