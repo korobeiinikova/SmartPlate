b520399f619f456c7d8f9c11c7b3758090ea7a1a
Darya Korobeynikova <dasha_kord@rambler.ru>
2026-04-16 00:34:47 +0700

    lab4

--- SmartPlate/Recipes/views.py
+++ SmartPlate/Recipes/views.py
@@ -54,24 +54,3 @@ def recipes_by_portions(request, count):
         'count': count,
     }
     return render(request, 'portion.html', context=data)
-
-
-def home(request):
-    # Страница для тестирования (не используется в основном сайте)
-    links = [
-        ("Главная Recipes", "/Recipes/"),
-        ("Категория breakfast", "/Recipes/cats/breakfast/"),
-        ("Категория dinner", "/Recipes/cats/dinner/"),
-        ("Категория lunch", "/Recipes/cats/lunch/"),
-        ("Категория desserts", "/Recipes/cats/desserts/"),
-        ("Категория breakfast с GET-параметром (max_calories=300)", "/Recipes/cats/breakfast/?max_calories=300"),
-        ("Рецепты на 4 порции", "/Recipes/portion/4/"),
-        ("Главная Users", "/Users/"),
-        ("Регистрация Users", "/Users/registration/"),
-        ("Профиль Users", "/Users/profile/"),
-    ]
-    html = "<h1>Ссылки для тестирования SmartPlate</h1><ul>"
-    for name, url in links:
-        html += f'<li><a href="{url}">{name}</a> — {url}</li>'
-    html += "</ul>"
-    return HttpResponse(html)
--- SmartPlate/SmartPlate/urls.py
+++ SmartPlate/SmartPlate/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
-from Recipes.views import page_not_found, home
+from django.views.generic import RedirectView
+from Recipes.views import page_not_found
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('Recipes/', include('Recipes.urls')),
     path('Users/', include('Users.urls')),
-    path('', home, name='home'),
+    path('', RedirectView.as_view(url='/Recipes/'), name='home'),
 ]
+
+handler404 = page_not_found
 
 handler404 = page_not_found
